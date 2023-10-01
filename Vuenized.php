fdb46a83c9938ad389d012175bbb1a3851a1b5b0
WIN 10 Desktop GVR <gsenandika@gmail.com>
2023-10-01 14:42:26 +0700

    add main script

---
+++ Vuenized.php
@@ -0,0 +1,24 @@
+<?php namespace App\Libraries\Ghivarra\Vuenized;
+
+/**
+ * Vuenized Library
+ *
+ * Created with love and proud by Ghivarra Senandika Rushdie
+ *
+ * @package Vuenized Library
+ *
+ * @var https://github.com/ghivarra
+ * @var https://facebook.com/bcvgr
+ * @var https://twitter.com/ghivarra
+ *
+**/
+
+class Vuenized
+{
+    public function getAssets()
+    {
+
+    }
+
+    //========================================================================================
+}
