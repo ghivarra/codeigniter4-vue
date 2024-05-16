c9b9163953f02e7e673733b072e6c3dbcbce9cae
Ghivarra Senandika Rushdie <61128334+ghivarra@users.noreply.github.com>
2024-05-16 13:14:05 +0700

    Create main.js file to replace original main.js from vue installation

---
+++ main.js
@@ -0,0 +1,26 @@
+import './assets/main.css'
+import { createApp, h } from 'vue'
+
+const Vuenized = (options) => {
+    let id = (typeof options.rootId === 'undefined') ? '#app' : `#${options.rootId}`
+    const pageData = JSON.parse(document.querySelector(id).getAttribute('data-page'))
+
+    const allPages = import.meta.glob('../../../views/**/*.vue');
+    const page = allPages[`../../../views/${pageData.view}.vue`]();
+
+    page.then((app) => {
+        options.setup(app.default, Object.assign({}, pageData.data), id)
+    })
+}
+
+// create new Vue on CodeIgniter 4
+Vuenized({
+    rootId: '#app',
+    setup: (App, props, root) => {
+        const app = createApp({
+            render: () => h(App, props)
+        })
+      
+        app.mount(root)
+    }
+})
