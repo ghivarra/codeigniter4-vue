7d3a32d9b60e513f74b438f76a28cf311463469e
Ghivarra Senandika Rushdie <61128334+ghivarra@users.noreply.github.com>
2024-05-16 13:21:11 +0700

    Update main.js

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ const Vuenized = (options) => {
     let id = (typeof options.rootId === 'undefined') ? '#app' : `#${options.rootId}`
     const pageData = JSON.parse(document.querySelector(id).getAttribute('data-page'))
 
-    const allPages = import.meta.glob('./views/**/*.vue');
-    const page = allPages[`./views/${pageData.view}.vue`]();
+    const allPages = import.meta.glob('./**/*.vue');
+    const page = allPages[`./${pageData.view}.vue`]();
 
     page.then((app) => {
         options.setup(app.default, Object.assign({}, pageData.data), id)
