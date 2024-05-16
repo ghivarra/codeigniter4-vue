c20f81ec7f23787b78cc533d8e6286819a801eb7
Ghivarra Senandika Rushdie <61128334+ghivarra@users.noreply.github.com>
2024-05-16 13:30:12 +0700

    Create vite.config.js for vite configurations

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+export default defineConfig(() => {
+	return {
+		plugins: [ vue() ],
+		build: {
+			emptyOutDir: false,
+      copyPublicDir: false,
+			outDir: 'public',
+			assetsDir: 'dist',
+			manifest: true,
+			rollupOptions: {
+				input: `./src/main.js`,
+			},
+		}
+		resolve: {
+			alias: {
+				'@': fileURLToPath(new URL(`./src`, import.meta.url))
+			}
+		}
+	};
+});
