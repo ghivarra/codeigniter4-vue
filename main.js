24ba16e5bf8d1fcbfc1b9b588711617cb910ba4a
Ghivarra Senandika Rushdie <61128334+ghivarra@users.noreply.github.com>
2025-05-20 12:38:15 +0700

    Update main.js
    update main.js so it can use custom root ID based on the dotenv file

--- main.js
+++ main.js
@@ -1,26 +1,41 @@
-import './assets/main.css'
 import { createApp, h } from 'vue'
 
 const Vuenized = (options) => {
     let id = (typeof options.rootId === 'undefined') ? '#app' : `#${options.rootId}`
+
     const pageData = JSON.parse(document.querySelector(id).getAttribute('data-page'))
+    const moduleName = `/${import.meta.env.VITE_RESOURCES_DIR}/views/${pageData.view}.vue`
 
-    const allPages = import.meta.glob('./**/*.vue');
-    const page = allPages[`./${pageData.view}.vue`]();
+    // uncomment this if using chunked
+    // const allPages = import.meta.glob('@/views/**/*.vue', { eager: false})
+    // const page = allPages[moduleName]()
 
-    page.then((app) => {
-        options.setup(app.default, Object.assign({}, pageData.data), id)
-    })
+    // uncomment this if using single file
+    const allPages = import.meta.glob('@/views/**/*.vue', { eager: true})
+    const page = allPages[moduleName]
+
+    if (typeof page.then === 'function') {
+
+        page.then((app) => {
+            options.setup(app.default, Object.assign({}, pageData.data), id)
+        })
+
+    } else {
+        
+        options.setup(page.default, Object.assign({}, pageData.data), id)
+    }
+
 }
 
 // create new Vue on CodeIgniter 4
 Vuenized({
-    rootId: '#app',
+    rootId: import.meta.env.VITE_APP_ID,
     setup: (App, props, root) => {
         const app = createApp({
             render: () => h(App, props)
         })
-      
+        
+        app.config.unwrapInjectedRef = true
         app.mount(root)
     }
 })
